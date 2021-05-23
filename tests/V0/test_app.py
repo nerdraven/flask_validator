9a5b723dcde33ce61b35cee667a7d5becf00261e
Damian <damianakpan2001@gmail.com>
2021-05-23 17:13:51 +0100

    Test app works

--- tests/V0/test_app.py
+++ tests/V0/test_app.py
@@ -1,3 +1,18 @@
+import pytest
+from flask.testing import FlaskClient
+from app.app import app
 
-def test_app_works():
-    assert "H" in "Hello World"
+
+@pytest.fixture()
+def test_client():
+    with app.test_client() as client:
+        yield client
+
+
+def test_app_works(test_client: FlaskClient):
+    rv = test_client.get('/')
+    assert b"Hello World" in rv.data
+
+
+def test_hi():
+    assert "1" in "1203"
