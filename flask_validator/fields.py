3489a0022de640691d7175e29bc5cdd8a35c16a6
Damian Akpan <damianakpan2001@gmail.com>
2021-05-24 20:16:58 +0100

    WIP: fields.py

--- flask_validator/fields.py
+++ flask_validator/fields.py
@@ -8,15 +8,24 @@ class BaseField(ABC):
         self.required = required
 
     @abstractmethod
-    def validate():
+    def validate(self):
         pass
+
+
+class FieldMeta(type):
+
+    def __new__(mcs, name, bases, attrs: dict):
+        print(mcs)
+        print(name)
+        cls = super().__new__()
+        return cls
 
 
 class StringField(BaseField):
 
-    def __init__(self, maxlength, **kwargs):
+    def __init__(self, /, max_length, **kwargs):
         super().__init__(**kwargs)
-        self.maxlength = maxlength
+        self.max_length = max_length
 
     def validate(self):
         pass
